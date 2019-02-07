9c534cd7d17d22a164c3c486037e3c043eece383
Mirek Behina <mirekbehina@seznam.cz>
2019-02-07 14:21:46 +0100

    update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ if ('serviceWorker' in navigator) {
 
 
 $(document).ready(function () {
-    
+
 
     $.backstretch("images/background-clouds.gif");
 
@@ -47,7 +47,7 @@ $(document).ready(function () {
 
             obdobi = $("#predpoved").val();
 
-            latitude = (response.city.coord.lat);
+            /*latitude = (response.city.coord.lat);
             longitude = (response.city.coord.lon);
 
             const options = {
@@ -77,6 +77,44 @@ $(document).ready(function () {
                     .setContent("teplota je " + response.list[0].main.temp + " stupňů")
                     .openOn(map);
             
+            });*/
+
+            var ctx = document.getElementById("myChart").getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                    datasets: [{
+                        label: '# of Votes',
+                        data: [12, 19, 3, 5, 2, 3],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
             });
 
 
@@ -138,7 +176,9 @@ $(document).ready(function () {
         hide();
     });
 
-    
+
+
+
 });
 
 
