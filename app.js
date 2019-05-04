42326d0f7155d50a217bd470280ef3bf4297709e
Mirek Behina <mirekbehina@seznam.cz>
2019-05-04 20:01:29 +0200

    update

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ if ('serviceWorker' in navigator) {
 
 $(document).ready(function () {
 
+    //window.localStorage.clear();
+
     var server = "https://api.openweathermap.org/data/2.5/forecast?q="
     var owm_key = "&lang=cz&APPID=afb6e76426a0802ed7f8dcdb42900eab&units=metric"
 
@@ -27,7 +29,7 @@ $(document).ready(function () {
     $('.vypln').submit(function (e) {
         searched_city = $('#mesto').val();
         e.preventDefault();
-        
+
         hide();
 
         var settings = {
@@ -35,8 +37,8 @@ $(document).ready(function () {
             "crossDomain": true,
             "url": server + searched_city + owm_key,
             "method": "GET",
-            success: function(response){
-                response_obtained = response; 
+            success: function (response) {
+                response_obtained = response;
 
             },
             error: function (e) {
@@ -47,15 +49,17 @@ $(document).ready(function () {
 
 
         $.ajax(settings).done(function (response) {
+
+            console.log(response);
             dump();
 
 
             search_flag = true;
 
-            localStorage.setItem(searched_city, JSON.stringify(response));
+
 
             //var getJSON_response = JSON.parse(localStorage.getItem(searched_city));
-            
+
             var arr_temp = [];
             var arr_dates = [];
             var i;
@@ -63,7 +67,7 @@ $(document).ready(function () {
                 arr_temp.push(response.list[i].main.temp);
                 let trimmed_date = (response.list[i].dt_txt).slice(11, -3);
                 arr_dates.push(trimmed_date);
-            } 
+            }
 
             var ctx = document.getElementById("myChart").getContext('2d');
             var myChart = new Chart(ctx, {
@@ -110,13 +114,14 @@ $(document).ready(function () {
                         }]
                     }
                 }
+
+
             });
 
 
             //NASTAVENI     //.serializeArray    var div = $("<div></div>");   div.append("ahoj");
+            $("#result_city").append(searched_city).css("text-transform", "lowercase");
 
-            $("#result_city").append(searched_city);
-            
             //teplota
             if ($("#teplota_radio").is(":checked") === true) {
                 let teplota = (response.list[0].main.temp).toString();
@@ -155,59 +160,98 @@ $(document).ready(function () {
 
 
             for (i = 0; i < 16; i++) {
-                    var table_date = response.list[i].dt_txt.slice(6,-3);
+                var table_date = response.list[i].dt_txt.slice(6, -3);
                 $('body #results_table_detail').append(
                     '<tr><td>' + table_date + '</td>' +
                     '<td>' + response.list[i].main.temp + '°C</td>' +
                     '<td>' + response.list[i].wind.speed + ' m/s</td>' +
                     '<td>' + response.list[i].weather[0].description + '</td>' +
                     '</tr>');
-                    
+
             }
 
+
+            $("#add_result_button").click(function() {
+                localStorage.setItem(searched_city, JSON.stringify(response));
+                console.log(searched_city);
+                $(".saved_results").append("<li>" + searched_city + "<span id='remove_result'></span></li>");
+            });
         });
     });
 
-    $("#map_button_open").click(function(){
-        
-        if(search_flag) {
+
+
+
+    //key_cislo pres regex mazat do vypisu vsechno za podtrzitkem v pripade ze hodnota klice je stejna pridat podrtzitko plus i navyssujici se hodnota
+
+
+    function clear_map_localstorage() {
+        var keysToRemove = ["UUID", "log2018", "lastSyncableUpdatedItem", "settings_country", "settings_firstUserSession", "settings_homeLocation", "settings_homeLocation_ts", "settings_ipLocation", "settings_sessionCounter201803", "settings_startUp", "webGLtest3"];
+
+        for (key of keysToRemove) {
+            window.localStorage.removeItem(key);
+        }
+        console.log("smazano");
+    };
+
+    clear_map_localstorage();
+
+
+    for (i = 0; i < window.localStorage.length; i++) {
+        $(".saved_results").append("<li class='mesto_vysledek_" + i +"'>" + window.localStorage.key(i) + "<span id='remove_result'></span></li>");
+    }
+
+    $(document).on("click", "li #remove_result", function () {
+        window.localStorage.removeItem($(this).closest("li").text());
+        $(this).closest("li").remove();
+    });
+
+
+
+    $("#map_button_open").click(function () {
+
+
+        if (search_flag) { //oddelit vykreslovaci funkci jinam a pak ji schovat pod podminku pokud uz mapa byla vykreslena :) 
 
             latitude = (response_obtained.city.coord.lat);
             longitude = (response_obtained.city.coord.lon);
+
 
             const options = {
 
                 // Required: API key
                 key: '6sZ6r5filmvn76tOGHe1sYXh140hwHiu',
-            
+
                 // Put additional console output
-                verbose: true,
-            
+                verbose: false,
+
                 // Optional: Initial state of the map
                 lat: latitude,
                 lon: longitude,
                 zoom: 5,
             }
-            
+
             // Initialize Windy API
             windyInit(options, windyAPI => {
                 // windyAPI is ready, and contain 'map', 'store',
                 // 'picker' and other usefull stuff
-            
+
                 const { map } = windyAPI
                 // .map is instance of Leaflet map
-            
+
                 L.popup()
                     .setLatLng([latitude, longitude])
                     .setContent(searched_city + ": " + response_obtained.list[0].main.temp)
                     .openOn(map);
-            
+
             });
 
 
             $("#windy").toggleClass("displayed_windy");
         }
-      });
+
+        clear_map_localstorage();
+    });
 
     $('.nastaveni').click(function () {
         $(this).toggleClass("nastaveni_rotate nastaveni_transition");
@@ -219,70 +263,74 @@ $(document).ready(function () {
         $(".overlay").toggleClass("displayed");
     };
 
+    $('#add_result_button').click(function () {
+
+    });
+
     $('.search_button').click(function () {
         hide();
     });
-
-    var colors = new Array(
-        [62,35,255],
-        [60,255,60],
-        [255,35,98],
-        [45,175,230],
-        [255,0,255],
-        [255,128,0]);
-      
-      var step = 0;
-      //color table indices for: 
-      // current color left
-      // next color left
-      // current color right
-      // next color right
-      var colorIndices = [0,1,2,3];
-      
-      //transition speed
-      var gradientSpeed = 0.0007;
-      
-      function updateGradient()
-      {
-        
-        if ( $===undefined ) return;
-        
-      var c0_0 = colors[colorIndices[0]];
-      var c0_1 = colors[colorIndices[1]];
-      var c1_0 = colors[colorIndices[2]];
-      var c1_1 = colors[colorIndices[3]];
-      
-      var istep = 1 - step;
-      var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-      var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-      var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-      var color1 = "rgb("+r1+","+g1+","+b1+")";
-      
-      var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-      var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-      var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-      var color2 = "rgb("+r2+","+g2+","+b2+")";
-      
-       $('body').css({
-         background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
-          background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
-        
-        step += gradientSpeed;
-        if ( step >= 1 )
-        {
-          step %= 1;
-          colorIndices[0] = colorIndices[1];
-          colorIndices[2] = colorIndices[3];
+    /*
+        var colors = new Array(
+            [62,35,255],
+            [60,255,60],
+            [255,35,98],
+            [45,175,230],
+            [255,0,255],
+            [255,128,0]);
           
-          //pick two new target color indices
-          //do not pick the same as the current one
-          colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-          colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+          var step = 0;
+          //color table indices for: 
+          // current color left
+          // next color left
+          // current color right
+          // next color right
+          var colorIndices = [0,1,2,3];
           
-        }
-      }
-      
-      setInterval(updateGradient,10);
+          //transition speed
+          var gradientSpeed = 0.0007;
+          
+          function updateGradient()
+          {
+            
+            if ( $===undefined ) return;
+            
+          var c0_0 = colors[colorIndices[0]];
+          var c0_1 = colors[colorIndices[1]];
+          var c1_0 = colors[colorIndices[2]];
+          var c1_1 = colors[colorIndices[3]];
+          
+          var istep = 1 - step;
+          var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+          var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+          var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+          var color1 = "rgb("+r1+","+g1+","+b1+")";
+          
+          var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+          var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+          var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+          var color2 = "rgb("+r2+","+g2+","+b2+")";
+          
+           $('body').css({
+             background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+              background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
+            
+            step += gradientSpeed;
+            if ( step >= 1 )
+            {
+              step %= 1;
+              colorIndices[0] = colorIndices[1];
+              colorIndices[2] = colorIndices[3];
+              
+              //pick two new target color indices
+              //do not pick the same as the current one
+              colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+              colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+              
+            }
+          }
+          
+          setInterval(updateGradient,10);*/
 });
 
 
