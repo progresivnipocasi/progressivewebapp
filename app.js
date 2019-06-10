f6e1c3dbd3421fc620ebe878b9222ce34b6d5de2
Mirek Behina <mirekbehina@seznam.cz>
2019-06-10 11:43:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -338,6 +338,7 @@ $(document).ready(function () {
     });
 
     $('.refresh_button').click(function() {
+        $(this).removeClass("nastaveni_rotate nastaveni_transition");
         $(this).addClass("nastaveni_rotate nastaveni_transition");
     });
 
